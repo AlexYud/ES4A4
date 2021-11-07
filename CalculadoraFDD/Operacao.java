4323b168e2daffbaa048b83e9f07fdcb4c8558a3
André Zarzur <andrezarzur@hotmail.com>
2021-11-07 18:26:37 -0300

    Implementação do teste que falha (2)

--- CalculadoraFDD/Operacao.java
+++ CalculadoraFDD/Operacao.java
@@ -1,19 +1,53 @@
 package Caculadora;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+
 import javax.swing.JOptionPane;
 
+
 public abstract class Operacao {
-	double primeiroNum;
-	double segundoNum;
-	double resultado;
-	int reply = JOptionPane.NO_OPTION;
-	
-	public Operacao() {
-		// Inputs
-		while(reply != JOptionPane.YES_OPTION) {
-			this.primeiroNum = Double.parseDouble(JOptionPane.showInputDialog("Digite um n�mero"));
-			this.segundoNum = Double.parseDouble(JOptionPane.showInputDialog("Digite mais um n�mero"));
-			reply = JOptionPane.showConfirmDialog(null, "Confirmar valores? (" + primeiroNum + " | " + segundoNum + ")", "Confirmação", JOptionPane.YES_NO_OPTION);
-		}
-	}
+    double primeiroNum;
+    double segundoNum;
+    double resultado;
+    int reply = JOptionPane.NO_OPTION;
+
+    public Operacao() {
+        // Inputs
+        while(reply != JOptionPane.YES_OPTION) {
+            Update();
+            this.primeiroNum = Double.parseDouble(JOptionPane.showInputDialog("Digite um número", this.primeiroNum));
+            Update2();
+            this.segundoNum = Double.parseDouble(JOptionPane.showInputDialog("Digite mais um número", this.segundoNum));
+            reply = JOptionPane.showConfirmDialog(null, "Confirmar valores? (" + primeiroNum + " | " + segundoNum + ")", "Confirmação", JOptionPane.YES_NO_OPTION);
+        }
+
+
+    }
+
+    public void Update() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://dweet.io/get/latest/dweet/for/coopcalcDarkEclipse%22)).build();
+        var body = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            .thenApply(HttpResponse::body)
+            .join();
+        String[] arr = body.split(""");
+        var op = arr[32];
+        this.primeiroNum = Double.parseDouble(op);
+    }
+
+    public void Update2() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://dweet.io/get/latest/dweet/for/coopcalcDarkEclipse%22)).build();
+        var body = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            .thenApply(HttpResponse::body)
+            .join();
+        String[] arr = body.split(""");
+
+        var op = arr[37];
+        this.segundoNum = Double.parseDouble(op);
+    }
+
 }
